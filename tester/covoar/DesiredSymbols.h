06a39db587cf5c980ee2bd56d5f634859781b075
Ryan Long <ryan.long@oarcorp.com>
2021-08-11 14:06:03 -0400

    DesiredSymbols.h: Initialize symbolInfo
    CID 1399627: Uninitialized pointer field
    
    Closes #4433

--- tester/covoar/DesiredSymbols.h
+++ tester/covoar/DesiredSymbols.h
@@ -179,6 +179,7 @@ namespace Coverage {
      */
     SymbolInformation() :
       baseAddress( 0 ),
+      sourceFile( NULL ),
       uncoveredBranches( NULL ),
       uncoveredRanges( NULL ),
       unifiedCoverageMap( NULL )
