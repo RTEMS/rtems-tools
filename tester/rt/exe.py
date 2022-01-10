RTEMS/rtems-tools

commit cfd5aa41e847752cd98cde65515df7ce45ff9665

tester/exe: Adjust timeouts by the step size

tester/rt/exe.py
@@ -116,12 +116,12 @@ class exe(object):
     def _monitor(self, timeout):
         output_length = self.output_length
         step = 0.25
-        period = timeout[0]
-        seconds = timeout[1]
+        period = timeout[0] * step
+        seconds = timeout[1] * step
         while self.process and period > 0 and seconds > 0:
             current_length = self.output_length
             if output_length != current_length:
-                period = timeout[0]
+                period = timeout[0] * step
             output_length = current_length
             if seconds < step:
                 seconds = 0

commit f73627e3642f1f8295d3fc2c11fa852d9718e734

tester/exe: Fix adjust timeouts by the step size
The bug was introduced by cfd5aa41e847752cd98cde65515df7ce45ff9665.

The code monitors the test and generates a timeout test failure.  There are two
layers of timeout, one for output and the other for a test running too long.

The test needs to generate some output within the first timeout period and the
second timeout detects if the test has run too long. The first timeout detects
a target has locked up. The second is for a test looping generating output.

With exe type tests the output is buffered in a separate thread.

tester/rt/exe.py
@@ -116,12 +116,12 @@ class exe(object):
     def _monitor(self, timeout):
         output_length = self.output_length
         step = 0.25
-        period = timeout[0] * step
-        seconds = timeout[1] * step
+        period = timeout[0] / step
+        seconds = timeout[1] / step
         while self.process and period > 0 and seconds > 0:
             current_length = self.output_length
             if output_length != current_length:
-                period = timeout[0] * step
+                period = timeout[0] / step
             output_length = current_length
             if seconds < step:
                 seconds = 0